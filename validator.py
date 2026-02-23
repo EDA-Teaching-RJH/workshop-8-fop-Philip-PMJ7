a857e61e114ae27941c11802110e57c469eeb98e
Philip-PMJ7 <pmj7@kent.ac.uk>
2026-02-23 11:41:24 +0000

    Parts 9 and Challenge Complete

--- validator.py
+++ validator.py
@@ -1,4 +1,5 @@
 import re
+import csv
 
 def is_valid_email(email):
     # Returns True if it matches our final rule from Part 4, otherwise False
@@ -8,10 +9,15 @@ def is_valid_email(email):
 
 def main():
     email = input("What's your email? ").strip()
+    name = input("What's your name? ").strip()
+
     if is_valid_email(email):
-        print("Valid")
+        print("Valid email. Saving to contacts...")
+        with open("contacts.csv", "a") as file:
+            writer = csv.DictWriter(file, fieldnames=["name", "email"])
+            writer.writerow({"name": name, "email": email})
     else:
-        print("Invalid")
+        print("Invalid email. Not saved.")
 
 if __name__ == "__main__":
  main()
